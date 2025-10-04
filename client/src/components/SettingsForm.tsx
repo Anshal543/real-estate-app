432ae7d375889021678c10678ef34b8c780b1733
Anshal543 <sherhasan543@gmail.com>
2025-10-04 23:53:14 +0500

    feat setting form

---
+++ client/src/components/SettingsForm.tsx
@@ -0,0 +1,133 @@
+import { SettingsFormData, settingsSchema } from "@/lib/schemas";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const SettingsForm = ({
+  initialData,
+  onSubmit,
+  userType,
+}: SettingsFormProps) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const form = useForm<SettingsFormData>({
+    resolver: zodResolver(settingsSchema),
+    defaultValues: initialData,
+  });
+
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+    if (editMode) {
+      form.reset(initialData);
+    }
+  };
+
+  const handleSubmit = async (data: SettingsFormData) => {
+    await onSubmit(data);
+    setEditMode(false);
+  };
+
+  return (
+    <div className="pt-8 pb-5 px-8">
+      <div className="mb-5">
+        <h1 className="text-xl font-semibold">
+          {`${userType.charAt(0).toUpperCase() + userType.slice(1)} Settings`}
+        </h1>
+        <p className="text-sm text-gray-500 mt-1">
+          Manage your account preferences and personal information
+        </p>
+      </div>
+      <div className="bg-white rounded-xl p-6">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-6"
+          >
+            {/* Name */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your name"
+                      disabled={!editMode}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email"
+                      disabled={!editMode}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Phone Number */}
+            <FormField
+              control={form.control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone Number</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your phone number"
+                      disabled={!editMode}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="pt-4 flex justify-between">
+              <Button
+                type="button"
+                onClick={toggleEditMode}
+                variant="secondary"
+              >
+                {editMode ? "Cancel" : "Edit"}
+              </Button>
+              {editMode && (
+                <Button type="submit" variant="default">
+                  Save Changes
+                </Button>
+              )}
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default SettingsForm;
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -16,7 +16,7 @@ export const api = createApi({
     },
   }),
   reducerPath: "api",
-  tagTypes: [],
+  tagTypes: ["Managers", "Tenants"],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_arg, _api, _extraOptions, fetchWithBaseQuery) => {
@@ -31,13 +31,16 @@ export const api = createApi({
               : `/tenants/${user.userId}`;
 
           let userDetailResponse = await fetchWithBaseQuery(endpoints);
-          if(userDetailResponse.error && userDetailResponse.error.status === 404){
+          if (
+            userDetailResponse.error &&
+            userDetailResponse.error.status === 404
+          ) {
             userDetailResponse = await createNewUserInDatabase(
               user,
               idToken,
               userRole,
               fetchWithBaseQuery
-            )
+            );
           }
           return {
             data: {
@@ -51,9 +54,29 @@ export const api = createApi({
         }
       },
     }),
+    updateTenantSettings: build.mutation<
+      Tenant,
+      { cognitoId: string } & Partial<Tenant>
+    >({
+      query: ({ cognitoId, ...updatedTenant }) => ({
+        url: `/tenants/${cognitoId}`,
+        method: "PUT",
+        body: updatedTenant,
+      }),
+      invalidatesTags: (result) => [{ type: "Tenants", id: result.id }],
+    }),
+    updateManagerSettings: build.mutation<
+      Manager,
+      { cognitoId: string } & Partial<Manager>
+    >({
+      query: ({ cognitoId, ...updatedManager }) => ({
+        url: `/managers/${cognitoId}`,
+        method: "PUT",
+        body: updatedManager,
+      }),
+      invalidatesTags: (result) => [{ type: "Managers", id: result.id }],
+    }),
   }),
 });
 
-export const {
-  useGetAuthUserQuery,
-} = api;
+export const { useGetAuthUserQuery, useUpdateTenantSettingsMutation, useUpdateManagerSettingsMutation } = api;
