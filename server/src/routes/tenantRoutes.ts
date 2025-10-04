1c63103b6d20861d77f7e4ebdc1a68af779fcfc8
Anshal543 <sherhasan543@gmail.com>
2025-10-04 18:01:12 +0500

    update route for tenant and manager

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import {getTenant,createTenant} from '../controllers/tenantController'
+import {getTenant,createTenant,updateTenant} from '../controllers/tenantController'
 
 const router = express.Router();
 
 
 router.get("/:cognitoId",getTenant)
+router.put("/:cognitoId", updateTenant);
 router.post("/",createTenant)
 
 export default router;
